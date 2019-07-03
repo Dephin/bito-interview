7e9742089f18a44055d05d8c2795109e14f251c2
Dephin <hlzyjfeng@gmail.com>
2019-07-03 17:46:12 +0800

    init

---
+++ vue-bito/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="subpage1" name="subpage1">
+        <subpage1></subpage1>
+      </el-tab-pane>
+      <el-tab-pane label="subpage2" name="subpage2">
+        <subpage2></subpage2>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import Subpage1 from '@/components/Subpage1'
+import Subpage2 from '@/components/Subpage2'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      activeName: 'subpage1'
+    }
+  },
+  methods: {
+    handleClick (tab, event) {
+      console.log(tab, event)
+    }
+  },
+  components: {
+    'Subpage1': Subpage1,
+    'Subpage2': Subpage2
+  }
+}
+</script>
+
+<style>
+  .el-tabs {
+    width: 1200px;
+    margin: auto;
+  }
+  html {
+    background: #CCCCCC;
+  }
+</style>
---
+++ vue-bito/src/components/Subpage1.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <el-card shadow="always">
+      <p>asdadadssasdasda</p>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'subpage1',
+  data () {
+    return {}
+  },
+  mounted: function () {
+  },
+  components: {
+  }
+}
+// export default {
+//   name: 'subpage1',
+//   mounted () {
+//     window.setInterval(() => {
+//       setTimeout(this.getData, 0)
+//     }, 30000)
+//   },
+//   methods: {
+//     getData () {}
+//   }
+// }
+</script>
+
+<style scoped>
+</style>
